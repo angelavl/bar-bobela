40d67da8b4b85f5ec31cc9de758b91686b50ef79
angelavl <angela.vi.lor@gmail.com>
2022-09-06 21:35:23 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>  
+  <WorkInProgress/>
+</template>
+
+<script>
+import WorkInProgress from './components/WorkInProgress.vue'
+
+export default {
+  name: 'App',
+  components: {
+    WorkInProgress
+  }
+}
+</script>
+
+<style>
+body {
+  height: 100%;
+  width: 100%;
+  margin:0;
+  position: relative;
+  display:flex;
+  background: url('@/assets/images/pattern_white.jpg');
+   background-size: cover;
+}
+
+/* #app {
+  display: flex;
+  height: 100%;
+  width: 100%;
+  position: relative;   
+
+
+ font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}  */
+</style>
---
+++ src/components/WorkInProgress.vue
@@ -0,0 +1,23 @@
+<template>  
+    <div class="work_in_progress">
+        <h1>holi</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'WorkInProgress'
+}
+</script>
+
+<style scoped>
+.work_in_progress {
+    width: 50%;
+    height: 50%;
+    background-color: white;
+    position: center;
+}
+
+</style>
+
+
